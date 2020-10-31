f1169a1ceba4e466528d07590ae122cf50a7da97
Hoffman-Joshua <jhoffman5@carthage.edu>
2020-10-30 23:09:57 -0500

    Init EF Core with Person entity

---
+++ Elkay-Coding-Assignment/Data/PeopleContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Elkay_Coding_Assignment.Models;
+
+namespace Elkay_Coding_Assignment.Data
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext (DbContextOptions<PeopleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Elkay_Coding_Assignment.Models.Person> Person { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().ToTable("Person");
+        }
+    }
+}
