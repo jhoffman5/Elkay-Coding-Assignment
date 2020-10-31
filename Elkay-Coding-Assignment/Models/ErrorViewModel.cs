e4db1e0e300bab339af685ee8c84755b81762c3a
Hoffman-Joshua <jhoffman5@carthage.edu>
2020-10-30 22:36:31 -0500

    ASP.NET core MVC template

---
+++ Elkay-Coding-Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Elkay_Coding_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
