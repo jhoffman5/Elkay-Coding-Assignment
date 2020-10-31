b9bc8c08e0d7cd33e72d6d71ee2e6d74a54b8096
Hoffman-Joshua <jhoffman5@carthage.edu>
2020-10-30 22:54:54 -0500

    Added Person Model

---
+++ Elkay-Coding-Assignment/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elkay_Coding_Assignment.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address1 { get; set; }
+        public string? Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
